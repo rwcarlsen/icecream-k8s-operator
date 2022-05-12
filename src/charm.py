775cbdbe7066bd0bdeb59a1a8e45f59ba6782de9
Robert Carlsen <robert.carlsen@canonical.com>
2022-05-12 15:04:21 -0600

    sort of works now

--- src/charm.py
+++ src/charm.py
@@ -4,20 +4,14 @@
 #
 # Learn more at: https://juju.is/docs/sdk
 
-"""Charm the service.
-
-Refer to the following post for a quick-start guide that will help you
-develop a new k8s charm using the Operator Framework:
-
-    https://discourse.charmhub.io/t/4208
-"""
 
 import logging
+import sys
 
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, BlockedStatus
 
 logger = logging.getLogger(__name__)
 
@@ -30,26 +24,39 @@ class IcecreamK8SOperatorCharm(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
         self.framework.observe(self.on.icecream_pebble_ready, self._on_icecream_pebble_ready)
-        #self.framework.observe(self.on.config_changed, self._on_config_changed)
-        #self.framework.observe(self.on.fortune_action, self._on_fortune_action)
+        self.framework.observe(self.on.nodes_relation_changed, self._on_peer_relation_changed)
+
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.fortune_action, self._on_fortune_action)
         self._stored.set_default(things=[])
 
     def _on_icecream_pebble_ready(self, event):
         container = event.workload
+
+        # install icecream on workload container
+        try:
+            process = container.exec(['apt', 'update', '-y'])
+            process.wait()
+            process = container.exec(['apt', 'install', '-y', 'icecc'], stdout=sys.stdout, stderr=sys.stderr)
+            process.wait()
+        except Exception as err:
+            logger.error('Failed to install package: {}'.format(err))
+            self.unit.status = BlockedStatus('failed icecc install')
+            return
+
         if self.unit.is_leader():
+            # start the scheduler
             pebble_layer = {
-                "summary": "icecream scheduler",
-                "description": "icecream distributed compiler scheduler daemon",
                 "services": {
                     "icecc-scheduler": {
                         "override": "replace",
-                        "summary": "icecream scheduler",
-                        "command": "icecc-scheduler",
+                        "summary": "icecream scheduler service",
+                        "command": "icecc-scheduler -vvv",
                         "startup": "enabled",
                     }
                 },
             }
-            container.add_layer("scheduler", pebble_layer, combine=True)
+            container.add_layer("icecc-scheduler", pebble_layer, combine=True)
 
             container.autostart()
             self.unit.status = ActiveStatus()
@@ -59,8 +66,7 @@ class IcecreamK8SOperatorCharm(CharmBase):
             scheduler_addr = str(self.model.get_binding(r).network.bind_address)
             r.data[self.app].update({'scheduler_addr': scheduler_addr})
 
-    def on_peer_relation_changed(self, event)
-        container = self.unit.get_container('icecream')
+    def _on_peer_relation_changed(self, event):
 
         scheduler_addr = None
         if 'scheduler_addr' in event.relation.data[self.app]:
@@ -68,18 +74,17 @@ class IcecreamK8SOperatorCharm(CharmBase):
 
         if scheduler_addr:
             pebble_layer = {
-                "summary": "icecream worker",
-                "description": "icecream compiler worker daemon (iceccd)",
                 "services": {
                     "iceccd": {
                         "override": "replace",
-                        "summary": "icecream worker",
-                        "command": "iceccd -s {}".format(scheduler_addr),
+                        "summary": "icecream worker service",
+                        "command": "iceccd -vvv -s {}".format(scheduler_addr),
                         "startup": "enabled",
                     }
                 },
             }
-            container.add_layer("worker", pebble_layer, combine=True)
+            container = self.unit.get_container('icecream')
+            container.add_layer("iceccd", pebble_layer, combine=True)
             container.replan()
             self.unit.status = ActiveStatus()
 
